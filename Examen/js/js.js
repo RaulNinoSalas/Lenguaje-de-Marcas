da2a54180bf014f5624f78363565fd43f99cacf5
RaulNinoSalas <32308547+RaulNinoSalas@users.noreply.github.com>
2018-03-05 10:45:23 +0100

    Add files via upload

--- Examen/js/js.js
+++ Examen/js/js.js
@@ -1,47 +1,47 @@
-var menuAct="info";
+var menuAct = "info";
 var menuAnt;
 
-//al cargar por completo la página...
-window.onload = function(){
+//al cargar la página
+window.onload = function () {
 
-	//Seleccion del menu
-	document.getElementById("info").onclick = function(){
+	//Seleccion de pestañas
+	document.getElementById("info").onclick = function () {
 		borrarTodo();
 		mostrarInfo();
 	}
-	document.getElementById("test").onclick = function(){
+	document.getElementById("test").onclick = function () {
 		borrarTodo();
 		mostrarUbi();
 	}
-	document.getElementById("cont").onclick = function(){
+	document.getElementById("cont").onclick = function () {
 		borrarTodo();
 		mostrarExam();
 	}
 
 }
 
-// Navegar por el menú superior
+// Para navegar por el menú superior
 
-function mostrarInfo(){
-	document.getElementById("info").style.textDecoration="underline";
-	document.getElementById("infoD").style.display="block";
+function mostrarInfo() {
+	document.getElementById("info").style.textDecoration = "underline";
+	document.getElementById("infoD").style.display = "block";
 }
 
-function mostrarUbi(){
-	document.getElementById("test").style.textDecoration="underline";
-	document.getElementById("testD").style.display="block";
+function mostrarUbi() {
+	document.getElementById("test").style.textDecoration = "underline";
+	document.getElementById("testD").style.display = "block";
 }
 
-function mostrarExam(){
-	document.getElementById("cont").style.textDecoration="underline";
-	document.getElementById("contD").style.display="block";
+function mostrarExam() {
+	document.getElementById("cont").style.textDecoration = "underline";
+	document.getElementById("contD").style.display = "block";
 }
 
-function borrarTodo(){
-	document.getElementById("info").style.textDecoration="none";
-	document.getElementById("test").style.textDecoration="none";
-	document.getElementById("cont").style.textDecoration="none";
-	document.getElementById("infoD").style.display="none";
-	document.getElementById("testD").style.display="none";
-	document.getElementById("contD").style.display="none";
+function borrarTodo() {
+	document.getElementById("info").style.textDecoration = "none";
+	document.getElementById("test").style.textDecoration = "none";
+	document.getElementById("cont").style.textDecoration = "none";
+	document.getElementById("infoD").style.display = "none";
+	document.getElementById("testD").style.display = "none";
+	document.getElementById("contD").style.display = "none";
 }
